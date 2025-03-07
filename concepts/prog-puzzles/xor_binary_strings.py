59e3d22a1480371c3fb0553b63668d3e4fbec5a7
Shannen Nazareno <nzrnshannen@gmail.com>
2025-03-07 21:15:16 +0800

    added initial code for xor

---
+++ concepts/prog-puzzles/xor_binary_strings.py
@@ -0,0 +1,11 @@
+def check_if_bin(str1, str2):
+    
+    return True # return bin version if binary else error message
+
+# def xor_func() to print xor of 2 bins
+
+
+
+str1 = input("Enter first string: ")
+str2 = input("Enter second string: ")
+
